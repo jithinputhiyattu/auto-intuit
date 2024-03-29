a0f0902c29cfc1616e82ba497afa1c756e095a64
Jithin Puthiyattu <jithinputhiyattu@gmail.com>
2024-03-29 12:11:59 +0530

    Utility class

--- autointuit-infra/src/main/java/com/dreamblitz/autointuit/infra/util/InfraUtils.java
+++ autointuit-infra/src/main/java/com/dreamblitz/autointuit/infra/util/InfraUtils.java
@@ -2,7 +2,7 @@ package com.dreamblitz.autointuit.infra.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
-import java.io.IOException;
+import org.apache.commons.io.IOUtils;
 
 @Component
 public class InfraUtils {
@@ -10,8 +10,8 @@ public class InfraUtils {
     public static <T> T getFromJson(String jsonFileName, Class<T> valueType) {
         ObjectMapper jackson = new ObjectMapper();
         try {
-            //String json = IOUtils.toString(InfraUtils.class.getResource(jsonFileName), "UTF-8");
-            return null;//jackson.readValue(json, valueType);
+            String json = IOUtils.toString(InfraUtils.class.getResource(jsonFileName), "UTF-8");
+            return jackson.readValue(json, valueType);
         } catch (Exception ioExp) {
           // Log error
         }
