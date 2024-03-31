0d82e6eb25a7afd516790d5fc25d43491d1359a7
Jithin Puthiyattu <jithinputhiyattu@gmail.com>
2024-03-31 14:36:48 +0530

    HideCommon

--- autointuit-domain/src/main/java/com/dreamblitz/autointuit/domain/entity/Hideable.java
+++ autointuit-domain/src/main/java/com/dreamblitz/autointuit/domain/entity/Hideable.java
@@ -40,42 +40,41 @@ public interface Hideable {
         return subList;
     }
 
-    default void hideCommonFromList(Object[] list) {
+    default boolean isValid (Object [] list, Field field) throws IllegalAccessException {
+        for(Object obj :list) {
+            if(field.get(obj) == null ) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    default int getEqualCount(Object [] list, Field field) throws IllegalAccessException {
+        int count =1;
+        for(int i = 1; i <list.length;i++){
+            if(field.get(list[0]).equals( field.get(list[i]))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    default void hideCommonFromList(Object[] list) {
         int size  = list.length;
-        if(size >1) {
+        if(size > 1) {
             Class<?> cls = list[0].getClass();
             Field[] fields = cls.getDeclaredFields();
             for (Field field : fields) {
                 try {
-                    boolean skip = false;
-                    for(Object obj :list) {
-                        if(field.get(obj) == null ) {
-                            skip = true;
-                            break;
+                    if(isValid(list, field)) {
+                        List<Object> subList  = getSubList(list, field);
+                        if(subList.size() == size) {
+                            hideCommonFromList(subList.toArray());
                         }
-                    }
-                    if(skip) {
-                        continue;
-                    }
-                    List<Object> subList  = new ArrayList<>();
-                    for(Object obj :list) {
-                        if(field.get(obj) instanceof Hideable) {
-                            subList.add(field.get(obj));
-                        }
-                    }
-                    if(subList.size() == size) {
-                        hideCommonFromList(subList.toArray());
-                    }
-                    int count =1;
-                    for(int i = 1; i <size;i++){
-                        if(field.get(list[0]).equals( field.get(list[i]))) {
-                            count++;
-                        }
-                    }
-                    if(count == size) {
-                        for(int i = 0; i <size;i++){
-                            field.set(list[i], null);
+                        if(size == getEqualCount(list, field)) {
+                            for(int i = 0; i <size;i++){
+                                field.set(list[i], null);
+                            }
                         }
                     }
                 } catch (IllegalAccessException e) {
@@ -83,6 +82,5 @@ public interface Hideable {
                 }
             }
         }
-
     }
 }
